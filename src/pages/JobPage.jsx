c820e9901a8d8a4f43e888ec14dcb9858cec8a07
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2024-08-25 23:03:37 +0100

    Added ans styled the content of Single job pages

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Spinner from '../components/Spinner';
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const JobPage = () => {
   const { id } = useParams();
@@ -11,8 +13,8 @@ const JobPage = () => {
     const fetchJob = async () => {
       try {
         const res = await fetch(`/api/jobs/${id}`);
-        console.log();
         const data = await res.json();
+        console.log(data);
         setJob(data);
       } catch (error) {
         console.log('Error fetching data', error);
@@ -23,7 +25,93 @@ const JobPage = () => {
 
     fetchJob();
   }, []);
-  return loading ? <Spinner /> : <h1>{job.title}</h1>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <div>
+      {' '}
+      <section>
+        <div class="container m-auto py-6 px-6">
+          <Link
+            to="/jobs"
+            class="text-indigo-500 hover:text-indigo-600 flex items-center"
+          >
+            <FaArrowLeft className="mr-2" /> Back to Job Listings
+          </Link>
+        </div>
+      </section>
+      <section class="bg-indigo-50">
+        <div class="container m-auto py-10 px-6">
+          <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <main>
+              <div class="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
+                <div class="text-gray-500 mb-4">{job.type}</div>
+                <h1 class="text-3xl font-bold mb-4">{job.title}</h1>
+                <div class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
+                  <i class="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
+                  <p class="text-orange-700">{job.location}</p>
+                </div>
+              </div>
+
+              <div class="bg-white p-6 rounded-lg shadow-md mt-6">
+                <h3 class="text-indigo-800 text-lg font-bold mb-6">
+                  Job Description
+                </h3>
+
+                <p class="mb-4">{job.description}</p>
+
+                <h3 class="text-indigo-800 text-lg font-bold mb-2">Salary</h3>
+
+                <p class="mb-4">{job.salary} / Year</p>
+              </div>
+            </main>
+
+            {/* <!-- Sidebar --> */}
+            <aside>
+              <div class="bg-white p-6 rounded-lg shadow-md">
+                <h3 class="text-xl font-bold mb-6">Company Info</h3>
+
+                <h2 class="text-2xl">NewTek Solutions</h2>
+
+                <p class="my-2">
+                  NewTek Solutions is a leading technology company specializing
+                  in web development and digital solutions. We pride ourselves
+                  on delivering high-quality products and services to our
+                  clients while fostering a collaborative and innovative work
+                  environment.
+                </p>
+
+                <hr class="my-4" />
+
+                <h3 class="text-xl">Contact Email:</h3>
+
+                <p class="my-2 bg-indigo-100 p-2 font-bold">
+                  contact@newteksolutions.com
+                </p>
+
+                <h3 class="text-xl">Contact Phone:</h3>
+
+                <p class="my-2 bg-indigo-100 p-2 font-bold">555-555-5555</p>
+              </div>
+
+              <div class="bg-white p-6 rounded-lg shadow-md mt-6">
+                <h3 class="text-xl font-bold mb-6">Manage Job</h3>
+                <a
+                  href="/add-job.html"
+                  class="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
+                  Edit Job
+                </a>
+                <button class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                  Delete Job
+                </button>
+              </div>
+            </aside>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default JobPage;
